b343ad8356f6340d1a9f4533f7958f182e59ca3b
Pro DesignTZ <prodesigntz@gmail.com>
2024-04-26 03:42:13 +0300

    starter basic with container

---
+++ src/data/footerData.js
@@ -0,0 +1,48 @@
+export const footerData = [
+    {
+      title: "Company",
+      links: [
+        {
+          name: "About",
+          link: "/about",
+        },
+        {
+          name: "Services",
+          link: "/services",
+        },
+      
+        {
+          name: "Contact",
+          link: "/contact",
+        },
+        {
+          name: "Blog",
+          link: "/blog",
+        },
+      ],
+    },
+  
+    {
+      title: "Important",
+      links: [
+     
+        {
+          name: "Help Center",
+          link: "/help-center",
+        },
+        {
+          name: "Privancy Policy",
+          link: "/privancy-policy",
+        },
+        {
+          name: "Terms & Condition",
+          link: "/terms-condition",
+        },
+        {
+          name: "Cancelation Policy",
+          link: "/cancelation-policy",
+        },
+      ],
+    },
+  ];
+  
---
+++ src/data/menuData.js
@@ -0,0 +1,26 @@
+export const menuData = [
+  {
+    name: "Home",
+    link: "/",
+  },
+  {
+    name: "About",
+    link: "/about",
+  },
+  {
+    name: "Programs",
+    link: "/programs",
+  },
+  {
+    name: "Events",
+    link: "/events",
+  },
+  {
+    name: "Contact",
+    link: "/contact",
+  },
+  {
+    name: "Blog",
+    link: "/blog",
+  },
+];
